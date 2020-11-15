f07d27102d8b6b16731a24def52ee4cea05ad929
Alejandro Rodriguez <alex.rdz.chavi@gmail.com>
2020-11-15 06:02:33 -0600

    Se agrego el menu

---
+++ source.py
@@ -0,0 +1,24 @@
+
+
+loop = 1
+
+while loop == 1:
+    
+    menu = int(input("Que accion deseas realizar\n 1.Registrar una venta\n 2. Consultar una venta\n 3. Salir\n"))
+    if menu == 1:
+        pass
+    
+    
+    if menu == 2:
+        pass
+    
+    
+    if menu == 3:
+        
+        print("Vuelve pronto :)")
+        loop = 2
+    
+    
+    else:
+        
+        print("Opcion no valida, ingrese una opcion presentada en el menu")
